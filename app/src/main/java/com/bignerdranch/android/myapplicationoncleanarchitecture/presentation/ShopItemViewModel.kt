20fa4722eb7326ca75703bfa892c0d6b17a3fe96
Vn0000 <workmailkonovalov@gmail.com>
2023-10-06 14:19:31 +0400

    Доделан viewmodel для ShopItemActivity.kt. А также в слушателит нажатий добавлены интенты запускающие 2 активити с передачей данных в каком режиме открывать активити.

--- app/src/main/java/com/bignerdranch/android/myapplicationoncleanarchitecture/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/myapplicationoncleanarchitecture/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.myapplicationoncleanarchitecture.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bignerdranch.android.myapplicationoncleanarchitecture.data.ShopListRepositoryImpl
 import com.bignerdranch.android.myapplicationoncleanarchitecture.domain.AddShopItemUseCase
@@ -10,13 +12,31 @@ import java.util.*
 
 class ShopItemViewModel : ViewModel() {
     private val repository = ShopListRepositoryImpl
+
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
 
     fun getShopItem(shopItemId: UUID) {
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     fun addShopItemUseCase(inputName: String?, inputCount: String?) {
@@ -26,6 +46,7 @@ class ShopItemViewModel : ViewModel() {
         if (fieldsValid) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
     }
 
@@ -34,8 +55,11 @@ class ShopItemViewModel : ViewModel() {
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
-            val shopItem = ShopItem(name, count, true)
-            editShopItemUseCase.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
@@ -54,14 +78,26 @@ class ShopItemViewModel : ViewModel() {
     private fun validateInput(name: String, count: Int): Boolean {
         var result = true
         if (name.isBlank()) {
-            // TODO: show error input name
+            _errorInputName.value = true
             result = false
         }
         if (count <= 0) {
-            // TODO: show error input count
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+
+    public fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    public fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 
 }
